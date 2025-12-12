56f5ff177086966dd3d3afd11c8565818b7ee737
chris9713 <chrisgowe@gmail.com>
2025-12-12 21:59:09 +0900

    app.py

--- app.py
+++ app.py
@@ -24,8 +24,8 @@ st.markdown("""
     
     /* 2. The actual dropzone box - Default State */
     [data-testid='stFileUploader'] section {
-        background-color: #f8f9fa; /* Light grey background */
-        border: 2px dashed #f8f9fa;
+        background-color: #262730; /* Light grey background */
+        border: 2px dashed #262730;
         border-radius: 12px;
         padding: 30px;
         transition: all 0.3s ease-in-out;
@@ -446,6 +446,7 @@ if api_key:
 else:
 
     st.warning("Please enter API Key in the sidebar.")
+
 
 
 
